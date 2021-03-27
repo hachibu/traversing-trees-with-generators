eb19431b77877f6e2e727a6b70d2f3686c09cc32
hachibu <raymondsohn@gmail.com>
2021-03-27 16:45:28 -0700

    Add Tree methods for map, reduce and filter
    Add Tree methods for map, reduce and filter.

--- src/tree.js
+++ src/tree.js
@@ -8,6 +8,25 @@ class Tree {
     }
   }
 
+  *[Symbol.iterator]() {
+    yield this
+    for (let child of this.children) {
+      yield* child
+    }
+  }
+
+  map(func) {
+    return [...this].map(func)
+  }
+
+  reduce(reducer, state) {
+    return [...this].reduce(reducer, state)
+  }
+
+  filter(predicate) {
+    return [...this].filter(predicate)
+  }
+
   *backtrack(max = Infinity) {
     let curr = this
     let iter = 0
@@ -16,49 +35,6 @@ class Tree {
       iter++
       curr = curr.parent
     }
-  }
-
-  *traverseDepthFirst() {
-    yield this
-    for (let child of this.children) {
-      yield* child.traverseDepthFirst()
-    }
-  }
-
-  *traverseBreadthFirst() {
-    let queue = []
-    queue.push(this)
-    while (queue.length > 0) {
-      let node = queue.shift()
-      yield node
-      for (let child of node.children) {
-        queue.push(child)
-      }
-    }
-  }
-
-  reduceDepthFirst(reducer, state) {
-    for (let node of this.traverseDepthFirst()) {
-      state = reducer(state, node)
-    }
-    return state
-  }
-
-  reduceBreadthFirst(reducer, state) {
-    for (let node of this.traverseBreadthFirst()) {
-      state = reducer(state, node)
-    }
-    return state
-  }
-
-  search(predicate) {
-    let result = []
-    for (let node of this.traverseDepthFirst()) {
-      if (predicate(node)) {
-        result.push(node)
-      }
-    }
-    return result
   }
 }
 
--- src/tree.test.js
+++ src/tree.test.js
@@ -3,45 +3,40 @@ const Tree = require('./tree')
 const createTree = () =>
   new Tree(1, [new Tree(2, [new Tree(4)]), new Tree(3)])
 
-test('backtrack', () => {
+test('traverses tree', () => {
   let tree = createTree()
-  let [leaf] = tree.search(node => node.children.length === 0)
-  let values = [...leaf.backtrack()].map(node => node.value)
-  expect(values).toEqual([4, 2, 1])
-})
-
-test('backtrack max', () => {
-  let tree = createTree()
-  let [leaf] = tree.search(node => node.children.length === 0)
-  let values = [...leaf.backtrack(2)].map(node => node.value)
-  expect(values).toEqual([4, 2])
-})
-
-test('traverse depth-first', () => {
-  let tree = createTree()
-  let values = [...tree.traverseDepthFirst()].map(node => node.value)
+  let values = []
+  for (let node of tree) {
+    values.push(node.value)
+  }
   expect(values).toEqual([1, 2, 4, 3])
 })
 
-test('traverse breadth-first', () => {
-  let tree = createTree()
-  let values = [...tree.traverseBreadthFirst()].map(node => node.value)
-  expect(values).toEqual([1, 2, 3, 4])
+describe('backtrack', () => {
+  test('backtracks to the root', () => {
+    let tree = createTree()
+    let [leaf] = tree.filter(node => node.children.length === 0)
+    let values = [...leaf.backtrack()].map(node => node.value)
+    expect(values).toEqual([4, 2, 1])
+  })
+
+  test('does not backtrack to the root', () => {
+    let tree = createTree()
+    let [leaf] = tree.filter(node => node.children.length === 0)
+    let values = [...leaf.backtrack(2)].map(node => node.value)
+    expect(values).toEqual([4, 2])
+  })
 })
 
-test('flatten tree values depth-first', () => {
-  let tree = createTree()
-  let reducer = (state, node) => state.concat(node.value)
-  expect(tree.reduceDepthFirst(reducer, [])).toEqual([1, 2, 4, 3])
-})
+describe('reduce', () => {
+  test('flattens tree values', () => {
+    let tree = createTree()
+    let reducer = (state, node) => state.concat(node.value)
+    expect(tree.reduce(reducer, [])).toEqual([1, 2, 4, 3])
+  })
 
-test('flatten tree values breadth-first', () => {
-  let tree = createTree()
-  let reducer = (state, node) => state.concat(node.value)
-  expect(tree.reduceBreadthFirst(reducer, [])).toEqual([1, 2, 3, 4])
-})
-
-test('sum tree values', () => {
-  let tree = createTree()
-  expect(tree.reduceDepthFirst((state, node) => state + node.value, 0)).toEqual(10)
+  test('sums tree values', () => {
+    let tree = createTree()
+    expect(tree.reduce((state, node) => state + node.value, 0)).toEqual(10)
+  })
 })
