6f375d9f9b5048bee075b8716ee9ea19653b8b17
hachibu <raymondsohn@gmail.com>
2021-05-15 09:50:31 -0700

    Refactor mocks.js

--- test/mocks.js
+++ test/mocks.js
@@ -1,24 +1,13 @@
-const tree = () =>
-  linkTreeNodes({
-    value: 1,
-    children: [
-      {
-        value: 2,
-        children: [{ value: 3, children: [] }],
-      },
-      {
-        value: 4,
-        children: [{ value: 5, children: [] }],
-      },
-    ],
-  });
+const node = (value, children = []) => ({ value, children });
 
-const linkTreeNodes = (node) => {
+const link = (node) => {
   for (let child of node.children) {
     child.parent = node;
-    linkTreeNodes(child);
+    link(child);
   }
   return node;
 };
+
+const tree = () => link(node(1, [node(2, [node(3)]), node(4, [node(5)])]));
 
 module.exports = { tree };
