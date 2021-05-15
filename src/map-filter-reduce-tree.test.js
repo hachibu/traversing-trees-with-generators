242da183a848f3b3a513fb3f839f2369b4f2f1cf
hachibu <raymondsohn@gmail.com>
2021-05-15 09:39:18 -0700

    Add mapTree and filterTree tests

--- src/map-filter-reduce-tree.test.js
+++ src/map-filter-reduce-tree.test.js
@@ -1,5 +1,21 @@
 const { tree } = require("./mocks");
-const { reduceTree } = require("./map-filter-reduce-tree");
+const { mapTree, filterTree, reduceTree } = require("./map-filter-reduce-tree");
+
+describe("mapTree", () => {
+  test("double values", () => {
+    let values = mapTree(tree(), (node) => node.value * 2);
+    expect(values).toEqual([2, 4, 6, 8, 10]);
+  });
+});
+
+describe("filterTree", () => {
+  test("find even values", () => {
+    let values = filterTree(tree(), (node) => node.value % 2 === 0).map(
+      (node) => node.value
+    );
+    expect(values).toEqual([2, 4]);
+  });
+});
 
 describe("reduceTree", () => {
   test("flatten tree", () => {
